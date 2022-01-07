44a620d627b4e8b4a552c95e9fd25823864456c9
noname_dev <noname_dev@naver.com>
2022-01-07 21:04:56 +0900

    ~175p

--- src/main/java/com/book/springbootjpa/domain/service/posts/PostsService.java
+++ src/main/java/com/book/springbootjpa/domain/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.book.springbootjpa.domain.service.posts;
 
 import com.book.springbootjpa.domain.posts.Posts;
 import com.book.springbootjpa.domain.posts.PostsRepository;
+import com.book.springbootjpa.web.dto.PostsListResponseDto;
 import com.book.springbootjpa.web.dto.PostsResponseDto;
 import com.book.springbootjpa.web.dto.PostsSaveRequestDto;
 import com.book.springbootjpa.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -29,10 +33,27 @@ public class PostsService {
         return id;
     }
 
+    @Transactional
+    public void delete (Long id){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+
     public PostsResponseDto findById (Long id) {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
     }
+
 }
